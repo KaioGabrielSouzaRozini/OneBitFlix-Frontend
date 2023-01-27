ca9eb3ae1e5b23b99e943476c199357662c04880
bituka <bitukacomk@gmail.com>
2023-01-27 11:46:06 -0300

    Criando a estrutura da senha

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,14 @@
 import Head from "next/head";
+import { useState } from "react";
 import { Button, Col, Container, Row } from "reactstrap";
 import Footer from "../src/components/common/footer";
 import HeaderAuth from "../src/components/common/headerAuth";
+import PasswordForm from "../src/components/profile/password";
 import UserForm from "../src/components/profile/user";
 import styles from "../styles/profile.module.scss";
 
 const UserInfo = function () {
+  const [form, setForm] = useState("userForm");
   return (
     <>
       <Head>
@@ -20,11 +23,27 @@ const UserInfo = function () {
           <p className={styles.title}>Minha Conta</p>
           <Row className="pt-3 pb-5">
             <Col md={4} className={styles.btnColumn}>
-              <Button className={styles.renderForm}>DADOS PESSOAIS</Button>
-              <Button className={styles.renderForm}>SENHA</Button>
+              <Button
+                className={styles.renderForm}
+                style={{ color: form === "userForm" ? "#ff0044" : "white" }}
+                onClick={() => {
+                  setForm("userForm");
+                }}
+              >
+                DADOS PESSOAIS
+              </Button>
+              <Button
+                style={{ color: form === "passwordForm" ? "#ff0044" : "white" }}
+                className={styles.renderForm}
+                onClick={() => {
+                  setForm("passwordForm");
+                }}
+              >
+                SENHA
+              </Button>
             </Col>
             <Col md>
-              <UserForm />
+              {form === "userForm" ? <UserForm /> : <PasswordForm />}
             </Col>
           </Row>
         </Container>
