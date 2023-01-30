d0fc89c6077b6a0f37fb6555b276c1af20473fd2
bituka <bitukacomk@gmail.com>
2023-01-30 14:27:49 -0300

    Criação da página de Search

---
+++ src/components/searchCard/index.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { CourseType } from "../../services/courseService";
+import styles from "./styles.module.scss";
+
+interface props {
+  course: CourseType;
+}
+
+const SearchCard = function ({ course }: props) {
+  return (
+    <>
+      <Link href={`/courses/${course.id}`} className={styles.normalText}>
+        <div className={styles.searchCard}>
+          <img
+            src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`}
+            alt={course.name}
+            className={styles.searchCardImg}
+          />
+          <p className={styles.searchCardTitle}>{course.name}</p>
+          <p className={styles.searchCardDescription}>{course.synopsis}</p>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default SearchCard;
