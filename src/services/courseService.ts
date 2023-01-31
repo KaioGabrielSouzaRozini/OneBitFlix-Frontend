cb57991d72077477e4c02e9ce34b0f6831d5b29b
bituka <bitukacomk@gmail.com>
2023-01-31 09:50:12 -0300

    Backend da página de curso

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -85,6 +85,38 @@ const courseService = {
       });
     return res;
   },
+  like: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .post(
+        `/likes`,
+        { courseId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .catch((error) => {
+        return error.response;
+      });
+    return res;
+  },
+  removeLike: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .delete(`/likes/${courseId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+    return res;
+  },
   getSearch: async (name: string) => {
     const token = sessionStorage.getItem("onebitflix-token");
 
@@ -97,6 +129,23 @@ const courseService = {
       .catch((error) => {
         return error.response;
       });
+    return res;
+  },
+  getEpisodes: async (id: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .get(`/courses/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+
+        return error.response;
+      });
+
     return res;
   },
 };
